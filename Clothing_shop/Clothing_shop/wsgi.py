c1c8a625962f2979b355581ae73770d44e65f11a
ds0903 <danilgrey15@gmail.com>
2024-05-02 22:36:50 +0300

    ⚡️Start

---
+++ Clothing_shop/Clothing_shop/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Clothing_shop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Clothing_shop.settings")
+
+application = get_wsgi_application()
